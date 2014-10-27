e7a8435efb29565e6faf248c084643104bed14ae
Jonathan Chou <jonathanchou@Jonathans-MacBook-Pro-2.local>
2014-10-27 15:08:07 -0700

    check does not work, edit does work

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,14 +8,25 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property NSMutableArray *listArray;
+@property (weak, nonatomic) IBOutlet UITableView *listTableView;
+@property NSIndexPath *checkedIndexPath;
+@property BOOL editButton;
+@property int editCount;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    self.editButton = NO;
+    self.editCount = 0;
     [super viewDidLoad];
+    self.listTableView.allowsMultipleSelection = YES;
+    //    self.listArray = [@[] mutableCopy];
+    self.listArray = [[NSMutableArray alloc]init];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +34,72 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.listArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"listCell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = self.listArray[indexPath.row];
+    
+    return cell;}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if(!self.checkedIndexPath){
+        UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        self.checkedIndexPath = indexPath;
+    }
+    
+    else if([self.checkedIndexPath isEqual:indexPath]){
+        self.checkedIndexPath = nil;
+        UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+        cell.accessoryType = UITableViewCellAccessoryNone;
+
+    }
+    
+    else{
+        UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    if(self.editButton)
+    {   [self.listArray removeObjectAtIndex:indexPath.row];
+        [self.listTableView reloadData];
+    }
+    
+}
+
+- (IBAction)onAddButtonPressed:(id)sender
+{
+    
+    if (![self.textField.text isEqualToString:@""])
+    {
+    NSString *list = self.textField.text;
+    [self.listArray addObject:list];
+    
+    [self.listTableView reloadData];
+    self.textField.text = nil;
+    [self.textField resignFirstResponder];
+    }
+}
+
+- (IBAction)editButtonPressed:(UINavigationItem *)editButton
+{
+    self.editCount++;
+    if(self.editCount%2){
+    self.editButton = YES;
+    editButton.title = @"Done";
+    }
+    else{
+        self.editButton = NO;
+        editButton.title = @"Edit";
+    }
+}
+
 
 @end
